0b6659059296c005358c46971360d803dc315b3e
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-06-30 02:16:23 +0530

    Created FlatCards

--- App.tsx
+++ App.tsx
@@ -1,31 +1,20 @@
 import React from 'react';
 import {
+  ScrollView,
   View,
-  Text,
-  StyleSheet,
-  useColorScheme,
 } from 'react-native';
+import FlatCard from './Components/FlatCard';
 const App = (): JSX.Element => {
-  const isDark = useColorScheme() === 'dark';
   return (
-    <View style={styles.container}>
-      <Text style={isDark ? styles.whiteText : styles.darkText}>
-        Hello World!
-      </Text>
+   
+    <View >
+      <ScrollView>
+     <FlatCard />
+     </ScrollView>
     </View>
+   
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  darkText: {
-    color: '#000000',
-  },
-  whiteText: {
-    color: '#FFFFFF',
-  },
-});
+
 export default App;
---
+++ Components/FlatCard.tsx
@@ -0,0 +1,55 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function FlatCard() {
+  return (
+    <View>
+      <Text style={styles.headingText}>FlatCard</Text>
+      <View style={styles.flatContainer}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text style={styles.CardText}> Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text style={styles.CardText}>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text style={styles.CardText}>Green</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+    marginTop: 10,
+  },
+  flatContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 6,
+    margin: 5,
+  },
+  cardOne: {
+    backgroundColor: 'red',
+  },
+  cardTwo: {
+    backgroundColor: 'blue',
+  },
+  cardThree: {
+    backgroundColor: 'green',
+  },
+  CardText: {
+    color: '#FFFFFF',
+  },
+});
